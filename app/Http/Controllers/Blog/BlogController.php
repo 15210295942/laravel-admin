9bbeef9c35743dc7bceef35691535c570fef4ec1
gaohc <conlecl@163.com>
2017-12-08 17:51:33 +0800

    'update'

--- app/Http/Controllers/Blog/BlogController.php
+++ app/Http/Controllers/Blog/BlogController.php
@@ -19,7 +19,7 @@ class BlogController extends Controller
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function actionBlogList(Request $request){
-dd('ddd');
+
         return view('blog.blog');
     }
 }
